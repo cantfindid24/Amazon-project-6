5e7104a33690f2873044b9c394f79c14a509f19a
Ankit <24@gmail.com>
2023-06-16 15:05:47 +0530

    8 us bootstrap Card class-Change Product list to bootstrap - Create Product and Rating Component

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Rating from './Rating';
+
+function Product(props) {
+  const { product } = props;
+  return (
+    <Card className="product" key={product._id}>
+      <Link to={`/product/${product._id}`}>
+        <img
+          src={product.image}
+          className="card-img-top"
+          alt={product.name}
+        ></img>
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Card.Text>${product.price}</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-// import logger from 'use-reducer-logger';
+// import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -46,20 +48,13 @@ export default function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product._id}>
-              <Link to={`/product/${product._id}`}>
-                <img src={product.image} alt={product.name}></img>
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product._id}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>{product.price}</p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
