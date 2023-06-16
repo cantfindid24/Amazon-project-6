cdfaa14efc3a5cf2ad9c2dd5f3a6fb260c1d4ea4
Ankit <24@gmail.com>
2023-06-16 09:40:51 +0530

    2 list of products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product._id}>
+              <a href={`/product/${product._id}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product._id}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>{product.price}</p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
