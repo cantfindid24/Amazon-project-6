925019e470edfc2377928782a2d37e02d6514890
Ankit <24@gmail.com>
2023-06-16 12:11:42 +0530

    5 use reducer instead of usestate to manage complex state

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,39 @@
-import React, { useEffect, useState } from 'react';
-// import data from '../data';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products'); //return object
-      console.log(result); // object
-      setProducts(result.data); //array of objects
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products'); //return object
+        // console.log(result); // object
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
     };
     fetchData();
   }, []);
