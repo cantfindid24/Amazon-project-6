82449b2e50a5139f6c9588ef44e7c406d3410121
Ankit <24@gmail.com>
2023-06-24 14:44:50 +0530

    14 used Store Provider in index.js ,App.js and Product Screen... Currently our cart store duplicate items..we need to improce it in next sections.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useReducer } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,6 +12,7 @@ import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
+import { Store } from '../context/Store';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -50,6 +51,14 @@ function ProductScreen(props) {
     };
     fetchData();
   }, [_id]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity: 1 },
+    });
+  };
 
   return loading ? (
     <LoadingBox />
@@ -110,7 +119,9 @@ function ProductScreen(props) {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="dark">Add to Cart</Button>
+                      <Button onClick={addToCartHandler} variant="dark">
+                        Add to Cart
+                      </Button>
                     </div>
                   </ListGroup.Item>
                 )}
